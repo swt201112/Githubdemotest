1963f1e70a271e2c84fca66c2803273081b5456c
Sebastian Knott <knott@informatik.uni-bonn.de>
2011-10-13 15:09:14 +0200

    Now in color.

--- src/Car.java
+++ src/Car.java
@@ -4,12 +4,14 @@ public class Car {
 	private String licensePlate;
 	private String owner;
 	private String address;
+	private String color;
 	
 	
-	public Car(String licensePlate, String owner, String address){
+	public Car(String licensePlate, String owner, String address, String color){
 		this.licensePlate = licensePlate;
 		this.owner = owner;
 		this.address = address;
+		this.color = color;
 	}
 	
 	public String getLicensePlate(){
--- src/Test.java
+++ src/Test.java
@@ -8,11 +8,11 @@ public class Test {
 	private static void testCase(){
 		
 		// Create new Test-Cars
-		Car car0 = new Car("BN-548", "Mickey Mouse", "Römerstraße 1");
-		Car car1 = new Car("BN-123", "Donald Duck", "Römerstraße 5");
-		Car car2 = new Car("K-481", "Tick Duck", "Kölnstraße 1");
-		Car car3 = new Car("BN-123", "Trick Duck", "Kölnstraße 1");
-		Car car4 = new Car("BN-123", "Track Duck", "Kölnstraße 1");
+		Car car0 = new Car("BN-548", "Mickey Mouse", "Römerstraße 1","blue");
+		Car car1 = new Car("BN-123", "Donald Duck", "Römerstraße 5","red");
+		Car car2 = new Car("K-481", "Tick Duck", "Kölnstraße 1","yellow");
+		Car car3 = new Car("BN-123", "Trick Duck", "Kölnstraße 1","green");
+		Car car4 = new Car("BN-123", "Track Duck", "Kölnstraße 1","pink");
 		
 		// Register at TrafficOffice
 		TrafficOffice.registerCar(car0);
